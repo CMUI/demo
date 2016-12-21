2d775ba9cc3c684e3c7ca01cc13f3821b3e559d2
cssmagic <cssmagic.cn@gmail.com>
2016-12-21 17:21:10 +0800

    Update building script.

--- demo/v2/gulpfile.js
+++ demo/v2/gulpfile.js
@@ -1,27 +1,27 @@
 'use strict'
 
-var IS_DEV = !(process.env.NODE_ENV === 'production')
+const IS_DEV = !(process.env.NODE_ENV === 'production')
 
-var _ = require('underscore')
+const _ = require('underscore')
 
-var gulp = require('gulp')
-var gulpfiles = require('gulpfiles')
+const gulp = require('gulp')
+const gulpfiles = require('gulpfiles')
 
-var path = require('path')
+const path = require('path')
 
-var PATH_STATIC_ROOT = './static/baixing'
-var PATH_DIST_JS = path.join(PATH_STATIC_ROOT, 'js/dist/')
-var PATH_DIST_CSS = path.join(PATH_STATIC_ROOT, 'css/dist/')
-var FILES_DIST_JS = path.join(PATH_DIST_JS, '*.js')
-var FILES_DIST_CSS = path.join(PATH_DIST_CSS, '*.css')
-var PATH_SRC_CSS = path.join(PATH_STATIC_ROOT, 'css/src/')
-var FILES_SRC_CSS = path.join(PATH_SRC_CSS, '*.styl')
+const PATH_STATIC_ROOT = './static/baixing'
+const PATH_DIST_JS = path.join(PATH_STATIC_ROOT, 'js/dist/')
+const PATH_DIST_CSS = path.join(PATH_STATIC_ROOT, 'css/dist/')
+const FILES_DIST_JS = path.join(PATH_DIST_JS, '*.js')
+const FILES_DIST_CSS = path.join(PATH_DIST_CSS, '*.css')
+const PATH_SRC_CSS = path.join(PATH_STATIC_ROOT, 'css/src/')
+const FILES_SRC_CSS = path.join(PATH_SRC_CSS, '*.styl')
 
-var PATH_THEME_ROOT = './theme'
-var PATH_DIST_BAIXING = path.join(PATH_THEME_ROOT, 'baixing')
-var FILES_DIST_BAIXING = path.join(PATH_DIST_BAIXING, '*.php')
-var PATH_SRC_BAIXING = path.join(PATH_THEME_ROOT, 'baixing/src')
-var FILES_SRC_BAIXING = path.join(PATH_SRC_BAIXING, '*.jedi')
+const PATH_THEME_ROOT = './theme'
+const PATH_DIST_BAIXING = path.join(PATH_THEME_ROOT, 'baixing')
+const FILES_DIST_BAIXING = path.join(PATH_DIST_BAIXING, '*.php')
+const PATH_SRC_BAIXING = path.join(PATH_THEME_ROOT, 'baixing/src')
+const FILES_SRC_BAIXING = path.join(PATH_SRC_BAIXING, '*.jedi')
 
 gulp.task('clean', gulpfiles.del({
 	glob: [
@@ -31,7 +31,7 @@ gulp.task('clean', gulpfiles.del({
 	],
 }))
 
-var jsSrcList = require('./building/config-js-combo').jsSrcList
+const jsSrcList = require('./building/config-js-combo').jsSrcList
 gulp.task('js', gulpfiles.concat({
 	rules: jsSrcList,
 	dest: PATH_DIST_JS,
@@ -47,7 +47,7 @@ gulp.task('css', gulpfiles.stylus({
 	}
 }))
 
-var jedi = require('./building/gulp-jedi')
+const jedi = require('./building/gulp-jedi')
 gulp.task('jedi', function () {
 	return gulp.src([
 			FILES_SRC_BAIXING,
@@ -70,13 +70,13 @@ gulp.task('dist', gulp.series([
 gulp.task('watch-css', function () {
 	return gulp.watch([
 		FILES_SRC_CSS,
-		'./node_modules/cmui-v2/src/**/*.styl',
+		'./node_modules/cmui/src/**/*.styl',
 	], gulp.parallel('css'))
 })
 gulp.task('watch-js', function () {
 	return gulp.watch([
 		'./static/baixing/js/src/demo.js',
-		'./node_modules/cmui-v2/src/**/*.js',
+		'./node_modules/cmui/src/**/*.js',
 	], gulp.parallel('js'))
 })
 gulp.task('watch-jedi', function () {
